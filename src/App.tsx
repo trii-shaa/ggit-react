233fce4d08422b7e315f7d9cd5a8a489650ddc16
Patricia <trishawx008@gmail.com>
2025-09-12 12:17:56 +0300

    verified for errors

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div> 
+      <Header />
+      <h1>This is my first app</h1>
+      <Footer />
+    </div>
+  );
+}
+ export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () =>{
+    return <p>footer</p>;
+};
+
+export default Footer;
